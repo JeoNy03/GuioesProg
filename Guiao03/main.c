d897def4eeac6baff46d19ad98fae171fd9fa500
JeoNy <joaoneves03@gmail.com>
2022-05-11 09:08:17 +0100

    update

--- Guiao03/main.c
+++ Guiao03/main.c
@@ -5,7 +5,28 @@ typedef struct paragem{
     int minutos;
 }paragem;
 
+void lista_paragens(char *nomeFicheiro) {
+    FILE *f;
+    paragem p;
+
+    f = fopen(nomeFicheiro, "rb");
+
+    if (f == NULL) {
+        printf("Erro ao abrir o ficheiro\n");
+        return;
+    }
+
+    while (fread(&p, sizeof(paragem), 1, f) == 1) {
+        printf("Paragem:%s\tMinutos:%d\n",p.nome, p.minutos);
+    };
+
+    fclose(f);
+}
+
 int main() {
-    printf("Hello, World!\n");
+
+    lista_paragens("cp_ex1.dat");
+
+    printf("\n");
     return 0;
 }
